03e574823e9151b8862a13b84ea99c76173d1850
mariajosesalazar <mariaj.salazarf@gmail.com>
2018-06-29 10:05:13 -0400

    login funcional y transicion de display's

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-/* Holiii acá va tu código también */
+window.onload = () => {
+  //item login
+  const btnLogin = document.getElementById('btnLoginIn').addEventListener('click', () => {
+    //input email
+    const email = document.getElementById('inputEmail').value;
+    //input pass
+    const pass = document.getElementById('inputPass').value;
+    //condiciones para pasar el login, mejorar la de email
+    if (pass.length <= 8 || email.length <= 5) {
+      document.getElementById('navbar').style.display = 'block';
+      document.getElementById('home').style.display = 'block';
+      document.getElementById('login').style.display = 'none';
+
+    } else {
+    document.getElementById('passError').innerHTML = 'Datos inválidos, intenta nuevamente.';
+    }
+  }) 
+  //navbar 
+
+}
