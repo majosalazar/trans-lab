421d648ed03f4cb4f9493564da315012e06d544f
mariajosesalazar <mariaj.salazarf@gmail.com>
2018-07-06 11:25:08 -0400

    local storage activado

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ window.onload = () => {
   const btnLogin = document.getElementById('btnLoginIn').addEventListener('click', () => {
     //input email
     const email = document.getElementById('inputEmail').value;
+    //guardar localmente el email
+    window.localStorage.setItem('emailpo', email);
     //input pass
     const pass = document.getElementById('inputPass').value;
     //condiciones para pasar el login, mejorar la de email
-    if (pass.length <= 8 || email.length <= 5) {
+    if (pass.length <= 8 || email === 'hola@ble.cl') {
       document.getElementById('navbar').style.display = 'block';
       document.getElementById('home').style.display = 'block';
       document.getElementById('login').style.display = 'none';
